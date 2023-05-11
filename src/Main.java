83ee9c69afea140794c53119b15fd83fbc3b9ba1
Galina <razumova-g@mail.ru>
2023-05-11 09:22:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 18526; // стоимость билета
+        int rub = 20; // количество рублей для одной бонусной мили
+        int miles = amount / rub;
+        System.out.println(amount + miles);
+
+
+               }
+    }
